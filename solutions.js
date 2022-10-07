a02aa2c2e91b2d960d3a6ab6bf85d8fa348a17b3
Gil Jacob Abadi <giljabadi@gmail.com>
2022-10-07 16:19:24 -0500

    completed ten-functions exercise

--- solutions.js
+++ solutions.js
@@ -6,18 +6,45 @@
  * **Note**: While normally it is good practice to wrap your javascript in an
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
- * If you don't know what an iffe is, don't worry about it :)
- *
- * This file is for defining the following functions. See README.md for more detail.
- * isTrue
- * isFalse
- * not
- * addOne
- * isEven
- * isIdentical
- * isEqual
- * or
- * and
- * concat
- */
+ * If you don't know what an iife is, don't worry about it :)
+*/
 
+//1.
+const isTrue = (x) => x ? true : false
+
+//2.
+const isFalse = (x) => x ? false : true
+
+//3.
+const not = (x) => {
+    if (x === false || x === null ||  x=== undefined || x === "" || x === 0) {
+        return true
+
+    } else if (isNaN(x)==true) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+//4.
+const addOne = (x) => parseInt(x)+1
+
+//5.
+const isEven = (x) => x%2==0 ? true : false
+
+//6.
+const isIdentical = (x, y) => x===y ? true : false
+
+//7.
+const isEqual = (x, y) => x==y ? true : false
+
+//8.
+const or = (x, y) => x || y
+
+//9.
+const and = (x, y) => x && y
+
+//10.
+const concat = (x, y) => x + y
